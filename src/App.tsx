689a6a5438ed03d3cfcb810920d41dc90e4610c6
hugosmoreira <hugo.n4@gmail.com>
2022-08-01 20:37:31 -0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import { ThemeProvider } from "styled-components";
-import { Button } from "./components/Button";
+import { BrowserRouter } from "react-router-dom";
+import { Router } from  '../src/Router';
 import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from "./styles/global";
 
 export function App() {
 
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary"/>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+        <GlobalStyle />
     </ThemeProvider>
   );
 }
