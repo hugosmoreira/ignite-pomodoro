d12ecf16ca637dd5c99779a3eb5a1bd1f66402af
hugosmoreira <hugo.n4@gmail.com>
2022-08-05 19:35:30 -0700

    update

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,48 @@
+import { HandPalm, Play } from 'phosphor-react'
+import { useContext } from 'react'
+import {
+  HomeContainer,
+  FormContainer,
+  StartCountdownButton,
+  StopCountdownButton,
+  CountDownContainer,
+} from './styles'
+
+
 export function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <HomeContainer>
+      <form action=''>
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <input id='task' />
+
+          <label htmlFor="minutesAmount">Durante</label>
+          <input id='minutesAmount' type="number" />
+
+
+          <span>Minutos.</span>
+
+        </FormContainer>
+
+
+        <CountDownContainer>
+          <span>0</span>
+          <span>0</span>
+          <span>:</span>
+          <span>0</span>
+          <span>0</span>
+
+
+        </CountDownContainer>
+
+        <button type='submit'>
+          <Play size={24} />
+        </button>
+       
+
+       
+      </form>
+    </HomeContainer>
   );
 }
